723fb54a1968fc6fbcc28f309ee77eba7c15cd8e
Clucas0311 <clucas0311@gmail.com>
2021-05-14 21:09:42 -0400

    Chore: Setting Up Mongoose

---
+++ config/db.js
@@ -0,0 +1,23 @@
+// mongodb connection
+// bring in mongoose to connect
+const mongoose = require('mongoose');
+// bring in the config package
+const config = require('config');
+// to get the mongoURI file use config.get
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+	try {
+		mongoose.connect(db, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		});
+		console.log('MongoDB Connected...');
+	} catch (err) {
+		console.log(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
+};
+
+module.exports = connectDB;
